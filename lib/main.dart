e7aba4e6faaab879c7fa81072d58c0484c7ef926
melvspace <amelvspace@gmail.com>
2024-01-28 15:14:11 +0300

    feat: add sentry

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:responsive_framework/responsive_framework.dart';
+import 'package:sentry_flutter/sentry_flutter.dart';
 import 'package:valentine/router.dart';
 import 'package:valentine/theme/theme.dart';
 
-void main() {
-  runApp(const MyApp());
+Future<void> main() async {
+  await SentryFlutter.init(
+    (options) {
+      options.dsn = 'https://93f36bd2e378e24a6596d76d036a4406@o493241.ingest.sentry.io/4506648831655936';
+      options.tracesSampleRate = 1.0;
+    },
+    appRunner: () => runApp(MyApp()),
+  );
 }
 
 class MyApp extends StatelessWidget {
